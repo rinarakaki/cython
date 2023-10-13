c92f59f9f4274d11a21f1557c06e3869d305b7a7
Rin Arakaki <rnarkkx@gmail.com>
2023-10-13 23:01:17 +0900

    Update cdef_syntax.pyx

--- tests/compile/cdef_syntax.pyx
+++ tests/compile/cdef_syntax.pyx
@@ -2,13 +2,13 @@
 
 # the following are valid syntax constructs and should not produce errors
 
-type x = i32
+type X = i32
 
 fn no_semi():
-    cdef i32 i
+    let i32 i
 
 fn with_semi():
-    cdef i32 i;
+    let i32 i;
 
 def use_cdef():
     &no_semi, &with_semi
