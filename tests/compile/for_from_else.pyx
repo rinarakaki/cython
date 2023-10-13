a9fcfd6a04c3dcb49bdca708943d47328379872a
Rin Arakaki <rnarkkx@gmail.com>
2023-10-13 16:05:14 +0900

    Update and rename forfromelse.pyx to for_from_else.pyx

--- tests/compile/for_from_else.pyx
+++ tests/compile/for_from_else.pyx
@@ -1,14 +1,14 @@
 # mode: compile
 
-cdef void spam():
-    cdef int i, j=0, k=0
-    for i from 0 <= i < 10:
+fn void spam():
+    let i32 i, j = 0, k = 0
+    for i in 0..=10:
         j = k
     else:
         k = j
 
     # new syntax
-    for 0 <= i < 10:
+    for 0 in 0..=10:
         j = i
     else:
         j = k
