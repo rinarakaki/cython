d1ad24c465a98b2b6410ae9b84845217fa4ff75d
Rin Arakaki <rnarkkx@gmail.com>
2023-10-13 15:08:16 +0900

    Update c_for_from_loop.pyx

--- tests/compile/c_for_from_loop.pyx
+++ tests/compile/c_for_from_loop.pyx
@@ -5,14 +5,17 @@ let object a, b, x
 
 for i in 0..10:
     pass
+
 for i in 1..=10:
     pass
+
 for i from 10 >= i > 0:
     pass
+
 for i from 10 > i >= 0:
     pass
 
-for x from 0 <= x <= 10:
+for x in 0..=10:
     pass
 
 for i from a <= i <= b:
@@ -25,7 +28,7 @@ for i from k * 42 <= i <= j / 18:
     pass
 
 while j:
-    for i from 0 <= i <= 10:
+    for i in 0..=10:
         continue
         break
     else:
