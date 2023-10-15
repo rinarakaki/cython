93af65ad475fd8202d57e69a8bfdecfdd56dac5b
rnarkk <rnarkkx@gmail.com>
2023-10-15 12:02:46 +0000

    Update

--- Cython/Compiler/Builtin.py
+++ Cython/Compiler/Builtin.py
@@ -280,18 +280,6 @@ builtin_types_table = [
 # resulting in unintuitive runtime behavior and segfaults.
 #    ("bool",    "PyBool_Type",     []),
 
-    ("isize",   "intptr_t",        []),
-    ("usize",   "uintptr_t",       []),
-    ("i8",      "int8_t",          []),
-    ("i16",     "int16_t",         []),
-    ("i32",     "int32_t",         []),
-    ("i64",     "int64_t",         []),
-    ("u8",      "uint8_t",         []),
-    ("u16",     "uint16_t",        []),
-    ("u32",     "uint32_t",        []),
-    ("u64",     "uint64_t",        []),
-    ("f32",     "float",           []),
-    ("f64",     "double",          []),
     ("int",     "PyInt_Type",      []),
     ("long",    "PyLong_Type",     []),
     ("float",   "PyFloat_Type",    []),
