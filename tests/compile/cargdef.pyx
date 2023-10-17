3d17af93e99d87d11d6c686a90b90bccb2c67a2f
Rin Arakaki <rnarkkx@gmail.com>
2023-10-17 20:34:29 +0900

    Update cargdef.pyx

--- tests/compile/cargdef.pyx
+++ tests/compile/cargdef.pyx
@@ -1,12 +1,12 @@
 # mode: compile
 
-def f(obj, int i, double f, char *s1, char s2[]):
+def f(obj, int i, f64 f, char *s1, char s2[]):
     pass
 
-cdef g(obj, int i, double f, char *s1, char s2[]):
+cdef g(obj, int i, f64 f, char *s1, char s2[]):
     pass
 
-cdef do_g(object (*func)(object, int, double, char*, char*)):
+cdef do_g(object (*func)(object, int, f64, char*, char*)):
     return func(1, 2, 3.14159, "a", "b")
 
 do_g(&g)
