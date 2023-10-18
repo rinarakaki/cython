a501b867220fdd9c3fa0603f0baf52eb41a274e1
Rin Arakaki <rnarkkx@gmail.com>
2023-10-19 00:10:44 +0900

    Update memory_layout_2.pyx

--- docs/examples/userguide/memoryviews/memory_layout_2.pyx
+++ docs/examples/userguide/memoryviews/memory_layout_2.pyx
@@ -1,6 +1,6 @@
 from cython cimport view
 
 # VALID
-cdef int[::view.indirect, ::1, :] a
-cdef int[::view.indirect, :, ::1] b
-cdef int[::view.indirect_contiguous, ::1, :] c
+cdef i32[::view.indirect, ::1, :] a
+cdef i32[::view.indirect, :, ::1] b
+cdef i32[::view.indirect_contiguous, ::1, :] c
