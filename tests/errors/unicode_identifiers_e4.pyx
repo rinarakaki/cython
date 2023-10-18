8dfc057fc94c9f543147f11f37dc9a545271a727
Rin Arakaki <rnarkkx@gmail.com>
2023-10-18 15:26:28 +0900

    Update unicode_identifiers_e4.pyx

--- tests/errors/unicode_identifiers_e4.pyx
+++ tests/errors/unicode_identifiers_e4.pyx
@@ -4,8 +4,8 @@
 cdef class C:
     # these two symbols "\u1e69" and "\u1e9b\u0323" normalize to the same thing
     # so the two attributes can't coexist
-    cdef int ṩomething
-    cdef double ẛ̣omething
+    cdef i32 ṩomething
+    cdef f64 ẛ̣omething
 
 _ERRORS = u"""
 7:13: Previous declaration is here
