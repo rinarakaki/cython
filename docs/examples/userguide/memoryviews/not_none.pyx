18f80923829ef2862715563ea2d9a81b299e9bdd
Rin Arakaki <rnarkkx@gmail.com>
2023-10-19 00:12:44 +0900

    Update not_none.pyx

--- docs/examples/userguide/memoryviews/not_none.pyx
+++ docs/examples/userguide/memoryviews/not_none.pyx
@@ -2,7 +2,6 @@ import numpy as np
 
 def process_buffer(i32[:,:] input_view not None,
                    i32[:,:] output_view=None):
-
    if output_view is None:
        # Creating a default view, e.g.
        output_view = np.empty_like(input_view)
