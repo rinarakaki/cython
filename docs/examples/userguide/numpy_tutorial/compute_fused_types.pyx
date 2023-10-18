d6ab3effcd12eae54e2fd7a0a0a53646a9722084
Rin Arakaki <rnarkkx@gmail.com>
2023-10-18 23:58:30 +0900

    Update compute_fused_types.pyx

--- docs/examples/userguide/numpy_tutorial/compute_fused_types.pyx
+++ docs/examples/userguide/numpy_tutorial/compute_fused_types.pyx
@@ -7,15 +7,12 @@ ctypedef fused my_type:
     double
     long long
 
-
 cdef my_type clip(my_type a, my_type min_value, my_type max_value):
     return min(max(a, min_value), max_value)
-
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def compute(my_type[:, ::1] array_1, my_type[:, ::1] array_2, my_type a, my_type b, my_type c):
-
     x_max = array_1.shape[0]
     y_max = array_1.shape[1]
 
@@ -32,11 +29,10 @@ def compute(my_type[:, ::1] array_1, my_type[:, ::1] array_2, my_type a, my_type
     cdef my_type[:, ::1] result_view = result
 
     cdef my_type tmp
-    cdef Py_ssize_t x, y
+    cdef isize x, y
 
     for x in range(x_max):
         for y in range(y_max):
-
             tmp = clip(array_1[x, y], 2, 10)
             tmp = tmp * a + array_2[x, y] * b
             result_view[x, y] = tmp + c
