1c29bae17aa9219a243b1291b75ecd84f665bfee
Rin Arakaki <rnarkkx@gmail.com>
2023-10-18 15:14:54 +0900

    Update fused_syntax_ctypedef.pyx

--- tests/errors/fused_syntax_ctypedef.pyx
+++ tests/errors/fused_syntax_ctypedef.pyx
@@ -2,7 +2,7 @@
 
 cimport cython
 
-ctypedef cython.fused_type(int, float) fused_t
+ctypedef cython.fused_type(i32, f32) fused_t
 
 _ERRORS = u"""
 fused_syntax_ctypedef.pyx:5:39: Syntax error in ctypedef statement
