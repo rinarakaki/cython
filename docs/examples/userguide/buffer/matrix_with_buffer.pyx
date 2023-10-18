b8a08befdda0803aebe9084b8624974182bcc033
Rin Arakaki <rnarkkx@gmail.com>
2023-10-18 23:42:37 +0900

    Update matrix_with_buffer.pyx

--- docs/examples/userguide/buffer/matrix_with_buffer.pyx
+++ docs/examples/userguide/buffer/matrix_with_buffer.pyx
@@ -7,7 +7,7 @@ cdef class Matrix:
     cdef Py_ssize_t ncols
     cdef Py_ssize_t[2] shape
     cdef Py_ssize_t[2] strides
-    cdef vector[float] v
+    cdef vector[f32] v
 
     def __cinit__(self, Py_ssize_t ncols):
         self.ncols = ncols
@@ -16,7 +16,7 @@ cdef class Matrix:
         """Adds a row, initially zero-filled."""
         self.v.resize(self.v.size() + self.ncols)
 
-    def __getbuffer__(self, Py_buffer *buffer, int flags):
+    def __getbuffer__(self, Py_buffer *buffer, i32 flags):
         cdef Py_ssize_t itemsize = sizeof(self.v[0])
 
         self.shape[0] = self.v.size() // self.ncols
@@ -25,14 +25,12 @@ cdef class Matrix:
         # Stride 1 is the distance, in bytes, between two items in a row;
         # this is the distance between two adjacent items in the vector.
         # Stride 0 is the distance between the first elements of adjacent rows.
-        self.strides[1] = <Py_ssize_t>(  <char *>&(self.v[1])
-                                       - <char *>&(self.v[0]))
-
-
+        self.strides[1] = <Py_ssize_t>(  <i8 *>&(self.v[1])
+                                       - <i8 *>&(self.v[0]))
 
         self.strides[0] = self.ncols * self.strides[1]
 
-        buffer.buf = <char *>&(self.v[0])
+        buffer.buf = <i8 *>&(self.v[0])
         buffer.format = 'f'                     # float
         buffer.internal = NULL                  # see References
         buffer.itemsize = itemsize
