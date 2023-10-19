7621c0ec04047692022188104fef5c34bad923d7
Rin Arakaki <rnarkkx@gmail.com>
2023-10-19 23:51:14 +0900

    Update indexing.pyx

--- docs/examples/userguide/fusedtypes/indexing.pyx
+++ docs/examples/userguide/fusedtypes/indexing.pyx
@@ -11,7 +11,6 @@ ctypedef fused fused_type2:
 cdef cfunc(fused_type1 arg1, fused_type1 arg2):
     print("cfunc called:", cython.typeof(arg1), arg1, cython.typeof(arg2), arg2)
 
-
 cpdef cpfunc(fused_type1 a, fused_type2 b):
     print("cpfunc called:", cython.typeof(a), a, cython.typeof(b), b)
 
