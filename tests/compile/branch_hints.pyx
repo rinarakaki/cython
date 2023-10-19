837386a46a77ebc2b269e4b18bb2a67c7305e242
Rin Arakaki <rnarkkx@gmail.com>
2023-10-19 16:58:18 +0900

    Update branch_hints.pyx

--- tests/compile/branch_hints.pyx
+++ tests/compile/branch_hints.pyx
@@ -3,7 +3,6 @@
 
 cimport cython
 
-
 @cython.test_assert_path_exists(
     "//IfClauseNode",
     "//IfClauseNode[not(@branch_hint)]",
@@ -11,7 +10,6 @@ cimport cython
 def if_simple(x):
     if x:
         x = 2
-
 
 @cython.test_assert_path_exists(
     "//IfClauseNode",
@@ -21,7 +19,6 @@ def if_return(x):
     if x:
         return 1
     raise TypeError()
-
 
 @cython.test_assert_path_exists(
     "//IfClauseNode",
@@ -33,7 +30,6 @@ def if_raise_else(x):
     else:
         return 1
 
-
 @cython.test_assert_path_exists(
     "//IfClauseNode",
     "//IfClauseNode[@branch_hint = 'likely']",
@@ -44,7 +40,6 @@ def if_else_raise(x):
     else:
         raise TypeError()
 
-
 @cython.test_assert_path_exists(
     "//IfClauseNode",
     "//IfClauseNode[@branch_hint = 'unlikely']",
@@ -54,7 +49,6 @@ def if_raise_else_raise(x):
         raise ValueError()
     else:
         raise TypeError()
-
 
 @cython.test_assert_path_exists(
     "//IfClauseNode",
@@ -71,7 +65,6 @@ def if_elif_raise_else_raise(x):
         raise AttributeError()
     else:
         raise TypeError()
-
 
 @cython.test_assert_path_exists(
     "//IfClauseNode",
