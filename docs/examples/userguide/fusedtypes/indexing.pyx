7382b52c9e61712f8eced28cd598203df2de2686
Rin Arakaki <rnarkkx@gmail.com>
2023-10-20 00:53:36 +0900

    Update indexing.pyx

--- docs/examples/userguide/fusedtypes/indexing.pyx
+++ docs/examples/userguide/fusedtypes/indexing.pyx
@@ -1,12 +1,12 @@
 cimport cython
 
 ctypedef fused fused_type1:
-    f64
-    f32
+    double
+    float
 
 ctypedef fused fused_type2:
-    f64
-    f32
+    double
+    float
 
 cdef cfunc(fused_type1 arg1, fused_type1 arg2):
     print("cfunc called:", cython.typeof(arg1), arg1, cython.typeof(arg2), arg2)
@@ -18,7 +18,7 @@ def func(fused_type1 a, fused_type2 b):
     print("func called:", cython.typeof(a), a, cython.typeof(b), b)
 
 # called from Cython space
-cfunc[f64](5.0, 1.0)
-cpfunc[f32, f64](1.0, 2.0)
+cfunc[double](5.0, 1.0)
+cpfunc[float, double](1.0, 2.0)
 # Indexing def function in Cython code requires string names
 func["float", "double"](1.0, 2.0)
