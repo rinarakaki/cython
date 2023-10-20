05f3d8aa02d9f1f0a376c53d971c6d91b8e2f26d
Rin Arakaki <rnarkkx@gmail.com>
2023-10-20 11:18:22 +0900

    Update type_checking.py

--- docs/examples/userguide/fusedtypes/type_checking.py
+++ docs/examples/userguide/fusedtypes/type_checking.py
@@ -1,20 +1,13 @@
-bunch_of_types = cython.fused_type(bytes, cython.int, cython.float)
-
-
-
-
-
+bunch_of_types = cython.fused_type(bytes, cython.i32, cython.f32)
 
 string_t = cython.fused_type(cython.p_char, bytes, unicode)
-
-
 
 @cython.cfunc
 def myfunc(i: cython.integral, s: bunch_of_types) -> cython.integral:
     # Only one of these branches will be compiled for each specialization!
-    if cython.integral is int:
+    if cython.integral is i32:
         print('i is an int')
-    elif cython.integral is long:
+    elif cython.integral is i64:
         print('i is a long')
     else:
         print('i is a short')
@@ -23,6 +16,6 @@ def myfunc(i: cython.integral, s: bunch_of_types) -> cython.integral:
         print("s is a string!")
     return i * 2
 
-myfunc(cython.cast(cython.int, 5), b'm')  # will print "i is an int" and "s is a string"
-myfunc(cython.cast(cython.long, 5), 3)    # will print "i is a long"
-myfunc(cython.cast(cython.short, 5), 3)   # will print "i is a short"
+myfunc(cython.cast(cython.i32, 5), b'm')  # will print "i is an int" and "s is a string"
+myfunc(cython.cast(cython.i64, 5), 3)    # will print "i is a long"
+myfunc(cython.cast(cython.i16, 5), 3)   # will print "i is a short"
