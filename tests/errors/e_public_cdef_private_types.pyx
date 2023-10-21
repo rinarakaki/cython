244def0e678539ccb7538a1c6040920aa898a4a1
rnarkk <rnarkkx@gmail.com>
2023-10-21 13:23:03 +0000

    Update

--- tests/errors/e_public_cdef_private_types.pyx
+++ tests/errors/e_public_cdef_private_types.pyx
@@ -34,7 +34,7 @@ cdef opt_func(x = None):
     pass
 
 _ERRORS = u"""
-e_public_cdef_private_types.pyx:8:22: Function declared public or api may not have private types
+e_public_cdef_private_types.pyx:8:19: Function declared public or api may not have private types
 e_public_cdef_private_types.pyx:11:19: Function declared public or api may not have private types
 e_public_cdef_private_types.pyx:14:5: Function declared public or api may not have private types
 e_public_cdef_private_types.pyx:17:5: Function declared public or api may not have private types
