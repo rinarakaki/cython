a731a36d916f6a1a9cd2580c5c8b4f97ac34eb62
Rin Arakaki <rnarkkx@gmail.com>
2023-10-22 16:37:34 +0900

    Update special_methods_T561_py38.pyx

--- tests/run/special_methods_T561_py38.pyx
+++ tests/run/special_methods_T561_py38.pyx
@@ -19,7 +19,7 @@ cdef class TwoOrThreeArgIPow:
     >>> print(ipow(TwoOrThreeArgIPow('a'), 'x', 'y'))
     a**x[y]
     """
-    let str name
+    cdef str name
 
     def __init__(self, name):
         self.name = name
@@ -39,7 +39,7 @@ cdef class ThreeArgIPow:
     >>> print(ipow(ThreeArgIPow('a'), 'x', 'y'))
     a**x[y]
     """
-    let str name
+    cdef str name
 
     def __init__(self, name):
         self.name = name
