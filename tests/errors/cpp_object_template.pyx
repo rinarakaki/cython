a7e179aa745bb95ea0d32cc82903658281d1071a
Rin Arakaki <rnarkkx@gmail.com>
2023-10-22 16:59:57 +0900

    Update cpp_object_template.pyx

--- tests/errors/cpp_object_template.pyx
+++ tests/errors/cpp_object_template.pyx
@@ -18,7 +18,7 @@ def memview():
     vmv.push_back(array.array("i", [1,2,3]))
 
 _ERRORS = u"""
-10:15: Python object type 'Python object' cannot be used as a template argument
+10:14: Python object type 'Python object' cannot be used as a template argument
 12:15: Python object type 'A' cannot be used as a template argument
 17:15: Reference-counted type 'int[:]' cannot be used as a template argument
 """
