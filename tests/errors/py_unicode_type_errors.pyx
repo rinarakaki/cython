b0fd594dfb323941ae9734b00cbacd579baf11f5
Rin Arakaki <rnarkkx@gmail.com>
2023-10-22 18:32:47 +0900

    Update py_unicode_type_errors.pyx

--- tests/errors/py_unicode_type_errors.pyx
+++ tests/errors/py_unicode_type_errors.pyx
@@ -21,5 +21,5 @@ _ERRORS = """
  8:23: Only single-character Unicode string literals or surrogate pairs can be coerced into Py_UCS4/Py_UNICODE.
 11:23: Only single-character Unicode string literals or surrogate pairs can be coerced into Py_UCS4/Py_UNICODE.
 14:23: Only single-character string literals can be coerced into ints.
-17:40: Bytes literals cannot coerce to Py_UNICODE/Py_UCS4, use a unicode literal instead.
+17:35: Bytes literals cannot coerce to Py_UNICODE/Py_UCS4, use a unicode literal instead.
 """
