f7e3934ed51662ed69dbf69657c7968b12a57eb7
Rin Arakaki <rnarkkx@gmail.com>
2023-10-22 14:15:48 +0900

    Update e_public_cdef_private_types.pyx

--- tests/errors/e_public_cdef_private_types.pyx
+++ tests/errors/e_public_cdef_private_types.pyx
@@ -36,7 +36,7 @@ cdef opt_func(x = None):
 _ERRORS = u"""
 e_public_cdef_private_types.pyx:8:14: Function declared public or api may not have private types
 e_public_cdef_private_types.pyx:11:19: Function declared public or api may not have private types
-e_public_cdef_private_types.pyx:14:5: Function declared public or api may not have private types
+e_public_cdef_private_types.pyx:14:0: Function declared public or api may not have private types
 e_public_cdef_private_types.pyx:17:5: Function declared public or api may not have private types
 e_public_cdef_private_types.pyx:20:21: Function with optional arguments may not be declared public or api
 e_public_cdef_private_types.pyx:23:21: Function with optional arguments may not be declared public or api
