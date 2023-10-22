4e96709dda9ac7ee6b74b0c8d57cc64b9df49db6
Rin Arakaki <rnarkkx@gmail.com>
2023-10-22 11:23:42 +0900

    Update Parsing.py

--- Cython/Compiler/Parsing.py
+++ Cython/Compiler/Parsing.py
@@ -2384,6 +2384,8 @@ def p_statement(s, ctx, first_statement = 0):
         cdef_flag = 1
         overridable = 1
         s.next()
+    elif s.sy in ("pub",):
+        cdef_flag = 1
     if cdef_flag:
         if ctx.level not in ('module', 'module_pxd', 'function', 'c_class', 'c_class_pxd'):
             s.error('cdef statement not allowed here')
