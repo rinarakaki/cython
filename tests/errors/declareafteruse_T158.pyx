34243c9bce236d9fd330b70b81f5166c12bd890d
Rin Arakaki <rnarkkx@gmail.com>
2023-10-22 17:00:20 +0900

    Update declareafteruse_T158.pyx

--- tests/errors/declareafteruse_T158.pyx
+++ tests/errors/declareafteruse_T158.pyx
@@ -55,7 +55,7 @@ cdef u128[100][100] var
 _ERRORS = u"""
 5:17: local variable 'vv' referenced before assignment
 6:17: local variable 's' referenced before assignment
-7:13: cdef variable 's' declared after it is used
+7:12: cdef variable 's' declared after it is used
 7:16: cdef variable 'vv' declared after it is used
 12:14: cdef variable 'i' declared after it is used
 18:14: cdef variable 'i' declared after it is used
