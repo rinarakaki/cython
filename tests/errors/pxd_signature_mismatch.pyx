58f8a722ecf0fcb2da7bb074627618a185760ed5
Rin Arakaki <rnarkkx@gmail.com>
2023-10-24 13:19:15 +0900

    Update pxd_signature_mismatch.pyx

--- tests/errors/pxd_signature_mismatch.pyx
+++ tests/errors/pxd_signature_mismatch.pyx
@@ -3,55 +3,55 @@
 
 # OK
 
-fn i32 wider_exception_check(i32 x, i32 x) except 0:
+fn i32 wider_exception_check(i32 x, i32 y) except 0:
     return 2
 
-fn i32 no_exception_raised(i32 x, i32 x):
+fn i32 no_exception_raised(i32 x, i32 y):
     return 2
 
-fn i32 any_exception_value1(i32 x, i32 x) except *:
+fn i32 any_exception_value1(i32 x, i32 y) except *:
     return 2
 
-fn i32 any_exception_value2(i32 x, i32 x) except -1:
+fn i32 any_exception_value2(i32 x, i32 y) except -1:
     return 2
 
-fn i32 any_exception_value3(i32 x, i32 x) except -2:
+fn i32 any_exception_value3(i32 x, i32 y) except -2:
     return 2
 
-fn i32 any_exception_value4(i32 x, i32 x) except? -2:
+fn i32 any_exception_value4(i32 x, i32 y) except? -2:
     return 2
 
-fn i32 optimised_exception_value(i32 x, i32 x) except *:  # => except? -1
+fn i32 optimised_exception_value(i32 x, i32 y) except *:  # => except? -1
     return 2
 
 
 # NOK
 
-fn i32 wrong_args(i32 x, i32 x):
+fn i32 wrong_args(i32 x, i32 y):
     return 2
 
-fn i32 wrong_return_type(i32 x, i32 x):
+fn i32 wrong_return_type(i32 x, i32 y):
     return 2
 
-fn i32 foreign_exception_value(i32 x, i32 x):
+fn i32 foreign_exception_value(i32 x, i32 y):
     return 2
 
-fn i32 narrower_exception_check(i32 x, i32 x) except? 0:
+fn i32 narrower_exception_check(i32 x, i32 y) except? 0:
     return 2
 
-fn i32 wrong_exception_value(i32 x, i32 x) except 1:
+fn i32 wrong_exception_value(i32 x, i32 y) except 1:
     return 2
 
-fn i32 wrong_exception_value_check(i32 x, i32 x) except? 1:
+fn i32 wrong_exception_value_check(i32 x, i32 y) except? 1:
     return 2
 
-fn i32 wrong_exception_value_optimised_check(i32 x, i32 x) except *:
+fn i32 wrong_exception_value_optimised_check(i32 x, i32 y) except *:
     return 2
 
-fn i32 wrong_exception_value_optimised(i32 x, i32 x) except *:
+fn i32 wrong_exception_value_optimised(i32 x, i32 y) except *:
     return 2
 
-fn i32 narrower_exception_check_optimised(i32 x, i32 x) except *:
+fn i32 narrower_exception_check_optimised(i32 x, i32 y) except *:
     return 2
 
 
