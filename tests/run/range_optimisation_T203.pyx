56bc4cb6ec817b1badb3b7e4ffc8000a9877a071
Rin Arakaki <rnarkkx@gmail.com>
2023-10-24 11:38:11 +0900

    Update range_optimisation_T203.pyx

--- tests/run/range_optimisation_T203.pyx
+++ tests/run/range_optimisation_T203.pyx
@@ -58,7 +58,7 @@ def for_from_bound_reassignment(i32 bound, int fake_bound):
         bound = fake_bound
     return i
 
-def for_from_step_reassignment(i32 bound, i32 bound, int fake_step):
+def for_from_step_reassignment(i32 bound, i32 step, int fake_step):
     """
     >>> for_from_step_reassignment(15, 5, 2)
     at 0
@@ -72,7 +72,7 @@ def for_from_step_reassignment(i32 bound, i32 bound, int fake_step):
         step = fake_step
     return i
 
-def for_from_target_reassignment(i32 bound, i32 bound):
+def for_from_target_reassignment(i32 bound, i32 factor):
     """
     >>> for_from_target_reassignment(10, 2)
     at 0
@@ -87,7 +87,7 @@ def for_from_target_reassignment(i32 bound, i32 bound):
         i *= factor
     return i
 
-def for_from_py_target_reassignment(i32 bound, i32 bound):
+def for_from_py_target_reassignment(i32 bound, i32 factor):
     """
     >>> for_from_py_target_reassignment(10, 2)
     at 0
@@ -102,7 +102,7 @@ def for_from_py_target_reassignment(i32 bound, i32 bound):
         i *= factor
     return i
 
-def for_from_py_global_target_reassignment(i32 bound, i32 bound):
+def for_from_py_global_target_reassignment(i32 bound, i32 factor):
     """
     >>> for_from_py_global_target_reassignment(10, 2)
     at 0
