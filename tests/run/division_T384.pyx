ded8319d3014c34843b3687756913c9bed1448bf
Rin Arakaki <rnarkkx@gmail.com>
2023-10-26 20:53:54 +0900

    Update division_T384.pyx

--- tests/run/division_T384.pyx
+++ tests/run/division_T384.pyx
@@ -9,7 +9,7 @@ use cython
 
 ctypedef isize index_t
 
-ctypedef double complex mycomplex
+ctypedef c64 mycomplex
 
 ctypedef struct MyStruct:
     mycomplex a, b
@@ -18,5 +18,5 @@ ctypedef struct MyStruct:
 def test(index_t x):
     let index_t y = x // 2
     let MyStruct s
-    s.a = x + y*1j
+    s.a = x + y * 1j
     return s.a
