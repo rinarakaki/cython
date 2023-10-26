9a4eafb920eb2c3a58dd394206ba4eb91888a2c6
Rin Arakaki <rnarkkx@gmail.com>
2023-10-26 20:32:56 +0900

    Update complex_cast_T445.pyx

--- tests/run/complex_cast_T445.pyx
+++ tests/run/complex_cast_T445.pyx
@@ -1,42 +1,42 @@
 # ticket: t445
 
-def complex_double_cast(f64 x, double complex z):
+def complex_double_cast(f64 x, c64 z):
     """
     >>> complex_double_cast(1, 4-3j)
     ((1+0j), (4-3j))
     """
-    let double complex xx = <double complex>x
-    let double complex zz = <double complex>z
+    let c64 xx = <c64>x
+    let c64 zz = <c64>z
     xx = x
     return xx, zz
 
-def complex_double_int_cast(i32 x, int complex z):
+def complex_double_int_cast(i32 x, c32 z):
     """
     >>> complex_double_int_cast(2, 2 + 3j)
     ((2+0j), (3+3j))
     """
-    let double complex xx = <double complex>x
-    let double complex zz = <double complex>(z+1)
+    let c64 xx = <c64>x
+    let c64 zz = <c64>(z + 1)
     return xx, zz
 
-def complex_int_double_cast(f64 x, double complex z):
+def complex_int_double_cast(f64 x, c64 z):
     """
     >>> complex_int_double_cast(2.5, 2.5 + 3.5j)
     ((2+0j), (2+3j))
     """
-    let i32 complex xx = <int complex>x
-    let i32 complex zz = <int complex>z
+    let c32 xx = <c32>x
+    let c32 zz = <c32>z
     return xx, zz
 
 cdef i32 side_effect_counter = 0
 
-fn double complex side_effect(double complex z):
+fn c64 side_effect(c64 z):
     global side_effect_counter
     side_effect_counter += 1
     print "side effect", side_effect_counter, z
     return z
 
-def test_side_effect(i32 complex z):
+def test_side_effect(c32 z):
     """
     >>> test_side_effect(5)
     side effect 1 (5+0j)
@@ -45,5 +45,5 @@ def test_side_effect(i32 complex z):
     side effect 2 (3-4j)
     (3-4j)
     """
-    let i32 complex zz = <int complex>side_effect(z)
+    let c32 zz = <c32>side_effect(z)
     return zz
