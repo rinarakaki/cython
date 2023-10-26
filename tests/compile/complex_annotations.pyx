671ddcca1808331c031c81377c12d243f6a19c7d
Rin Arakaki <rnarkkx@gmail.com>
2023-10-26 20:56:19 +0900

    Update complex_annotations.pyx

--- tests/compile/complex_annotations.pyx
+++ tests/compile/complex_annotations.pyx
@@ -3,5 +3,5 @@
 # Complex numbers defined in annotations weren't having their utility code imported directly
 # leading to compile-errors that the type wasn't defined. The test is intentionally minimal since
 # anything more thorough ends up creating the utility code
-fn f(x: complex):
+fn f(x: c32):
     pass
