0a882a3adcd52d4a6b817a5434da2b52fb1d6b3f
Rin Arakaki <rnarkkx@gmail.com>
2023-10-26 20:56:41 +0900

    Update complex_decorators.pyx

--- tests/compile/complex_decorators.pyx
+++ tests/compile/complex_decorators.pyx
@@ -5,6 +5,6 @@ use cython
 # Complex numbers defined in "cython.locals" weren't having their utility code imported directly
 # leading to compile-errors that the type wasn't defined. The test is intentionally minimal since
 # anything more thorough ends up creating the utility code
-@cython.locals(x=complex)
+@cython.locals(x=c32)
 fn f(x):
     pass
