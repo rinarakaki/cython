cc31ed6def15c7aece40a28b54fbf8d7f462af26
Rin Arakaki <rnarkkx@gmail.com>
2023-10-27 23:17:26 +0900

    Update closures_T82.pyx

--- tests/run/closures_T82.pyx
+++ tests/run/closures_T82.pyx
@@ -214,7 +214,7 @@ def deep_inner():
     "//DefNode//DefNode//DefNode[@needs_outer_scope = False]",  # a()
     "//DefNode//DefNode//DefNode[@needs_closure = False]",  # a(), g(), h()
 )]
-#[cython.test_fail_if_path_exists("//DefNode//DefNode//DefNode[@needs_closure = True]") # a(), g(), h()]
+#[cython.test_fail_if_path_exists("//DefNode//DefNode//DefNode[@needs_closure = True]")]  # a(), g(), h()
 def deep_inner_sibling():
     """
     >>> deep_inner_sibling()()
