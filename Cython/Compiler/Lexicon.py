0e3f7b1477d456cc1e52f90733574a64f86e768f
Rin Arakaki <rnarkkx@gmail.com>
2023-10-27 18:27:00 +0900

    Update Lexicon.py

--- Cython/Compiler/Lexicon.py
+++ Cython/Compiler/Lexicon.py
@@ -98,7 +98,7 @@ def make_lexicon():
 
         (beginstring, Method('begin_string_action')),
 
-        (comment + lineterm, IGNORE),
+        (comment, IGNORE),
         (spaces, IGNORE),
         (escaped_newline, IGNORE),
 
