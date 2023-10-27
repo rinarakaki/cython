fd367422c3284305a55c26d1c1ca686054f0ba28
Rin Arakaki <rnarkkx@gmail.com>
2023-10-27 23:40:36 +0900

    Update builtin_sorted.pyx

--- tests/run/builtin_sorted.pyx
+++ tests/run/builtin_sorted.pyx
@@ -1,19 +1,15 @@
 use cython
 
-
 def generator():
     yield 2
     yield 1
     yield 3
 
-
 def returns_set():
     return {"foo", "bar", "baz"}
 
-
 def returns_tuple():
     return (1, 2, 3, 0)
-
 
 #[cython.test_fail_if_path_exists("//SimpleCallNode")]
 def sorted_arg(x):
@@ -35,7 +31,6 @@ def sorted_arg(x):
     """
     return sorted(x)
 
-
 #[cython.test_assert_path_exists("//GeneralCallNode")]
 def sorted_arg_with_key(x):
     """
@@ -54,9 +49,8 @@ def sorted_arg_with_key(x):
     """
     return sorted(x, key=lambda x: -x)
 
-
-@cython.test_fail_if_path_exists("//YieldExprNode",
-                                 "//NoneCheckNode")
+#[cython.test_fail_if_path_exists("//YieldExprNode",
+                                  "//NoneCheckNode")]
 #[cython.test_assert_path_exists("//InlinedGeneratorExpressionNode")]
 def sorted_genexp():
     """
@@ -65,7 +59,6 @@ def sorted_genexp():
     """
     return sorted(i*i for i in range(10,0,-1))
 
-
 #[cython.test_fail_if_path_exists("//SimpleCallNode//SimpleCallNode")]
 #[cython.test_assert_path_exists("//SimpleCallNode/NameNode[@name = 'range']")]
 def sorted_list_of_range():
@@ -73,8 +66,7 @@ def sorted_list_of_range():
     >>> sorted_list_of_range()
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     """
-    return sorted(list(range(10,0,-1)))
-
+    return sorted(list(range(10, 0, -1)))
 
 #[cython.test_fail_if_path_exists("//SimpleCallNode")]
 def sorted_list_literal():
@@ -83,7 +75,6 @@ def sorted_list_literal():
     [1, 1, 2, 2, 3, 3]
     """
     return sorted([3, 1, 2] * 2)
-
 
 #[cython.test_fail_if_path_exists("//SimpleCallNode")]
 def sorted_tuple_literal():
