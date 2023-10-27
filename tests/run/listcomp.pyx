7e587e9fadded9e1fabdb37a2426f46df8148ec8
Rin Arakaki <rnarkkx@gmail.com>
2023-10-27 23:32:36 +0900

    Update listcomp.pyx

--- tests/run/listcomp.pyx
+++ tests/run/listcomp.pyx
@@ -3,7 +3,6 @@
 
 use cython
 
-
 def smoketest():
     """
     >>> smoketest()
@@ -13,7 +12,6 @@ def smoketest():
     result = [x*2 for x in range(5) if x % 2 == 0]
     assert x != 'abc'
     return result
-
 
 def list_genexp():
     """
@@ -25,7 +23,6 @@ def list_genexp():
     assert x == 'abc'
     return result
 
-
 def int_runvar():
     """
     >>> int_runvar()
@@ -33,7 +30,6 @@ def int_runvar():
     """
     let i32 x
     print [x*2 for x in range(5) if x % 2 == 0]
-
 
 cdef class A:
     def __repr__(self): return u"A"
@@ -46,7 +42,6 @@ def typed():
     let A obj
     print [obj for obj in [A(), A(), A()]]
 
-
 def inferred_type():
     """
     >>> inferred_type()
@@ -54,14 +49,12 @@ def inferred_type():
     """
     print [cython.typeof(obj) for obj in [A(), A(), A()]]
 
-
 def not_inferred_type():
     """
     >>> not_inferred_type()
     ['Python object', 'Python object', 'Python object']
     """
     print [cython.typeof(obj) for obj in [1, A(), 'abc']]
-
 
 def iterdict():
     """
@@ -73,7 +66,6 @@ def iterdict():
     l.sort()
     print l
 
-
 listcomp_result = [ i*i for i in range(5) ]
 def global_listcomp():
     """
@@ -83,7 +75,6 @@ def global_listcomp():
     [0, 1, 4, 9, 16]
     """
 
-
 class ListCompInClass(object):
     """
     >>> x = ListCompInClass()
@@ -91,7 +82,6 @@ class ListCompInClass(object):
     [1, 2, 3]
     """
     listcomp = [i+1 for i in range(3)]
-
 
 cdef class ListCompInCClass:
     """
@@ -101,7 +91,6 @@ cdef class ListCompInCClass:
     """
     listcomp = [i+1 for i in range(3)]
 
-
 def nested_result():
     """
     >>> nested_result()
@@ -109,7 +98,6 @@ def nested_result():
     """
     result = [[a-1 for a in range(b)] for b in range(4)]
     return result
-
 
 def listcomp_as_condition(sequence):
     """
@@ -124,7 +112,6 @@ def listcomp_as_condition(sequence):
         return true
     return false
 
-
 #[cython.test_fail_if_path_exists("//SimpleCallNode//ComprehensionNode")]
 #[cython.test_assert_path_exists("//ComprehensionNode")]
 def sorted_listcomp(sequence):
@@ -138,9 +125,8 @@ def sorted_listcomp(sequence):
     """
     return sorted([ n+1 for n in sequence ])
 
-
-@cython.test_fail_if_path_exists("//IfStatNode",
-                                 "//ComprehensionAppendNode")
+#[cython.test_fail_if_path_exists("//IfStatNode",
+                                  "//ComprehensionAppendNode")]
 #[cython.test_assert_path_exists("//ComprehensionNode")]
 def listcomp_const_condition_false():
     """
@@ -149,9 +135,8 @@ def listcomp_const_condition_false():
     """
     return [x*2 for x in range(3) if False]
 
-
-@cython.test_fail_if_path_exists("//IfStatNode",
-                                 "//ComprehensionAppendNode")
+#[cython.test_fail_if_path_exists("//IfStatNode",
+                                  "//ComprehensionAppendNode")]
 #[cython.test_assert_path_exists("//ComprehensionNode")]
 def listcomp_const_condition_false_bool_test():
     """
@@ -160,9 +145,8 @@ def listcomp_const_condition_false_bool_test():
     """
     return not [l for l in [1] if False]
 
-
-@cython.test_fail_if_path_exists("//IfStatNode",
-                                 "//ComprehensionAppendNode")
+#[cython.test_fail_if_path_exists("//IfStatNode",
+                                  "//ComprehensionAppendNode")]
 #[cython.test_assert_path_exists("//ComprehensionNode")]
 def listcomp_const_condition_false_assert():
     """
@@ -170,11 +154,10 @@ def listcomp_const_condition_false_assert():
     """
     assert not [l for l in [1] if False]
 
-
-@cython.test_fail_if_path_exists("//ComprehensionNode//IfStatNode",
-                                 "//ComprehensionAppendNode")
-@cython.test_assert_path_exists("//ComprehensionNode",
-                                "//IfStatNode")
+#[cython.test_fail_if_path_exists("//ComprehensionNode//IfStatNode",
+                                  "//ComprehensionAppendNode")]
+#[cython.test_assert_path_exists("//ComprehensionNode",
+                                 "//IfStatNode")]
 def listcomp_const_condition_false_if():
     """
     >>> listcomp_const_condition_false_if()
@@ -184,11 +167,10 @@ def listcomp_const_condition_false_if():
         return true
     return false
 
-
-@cython.test_fail_if_path_exists("//ComprehensionNode//IfStatNode",
-                                 "//ComprehensionAppendNode")
-@cython.test_assert_path_exists("//ComprehensionNode",
-                                "//IfStatNode")
+#[cython.test_fail_if_path_exists("//ComprehensionNode//IfStatNode",
+                                  "//ComprehensionAppendNode")]
+#[cython.test_assert_path_exists("//ComprehensionNode",
+                                 "//IfStatNode")]
 def listcomp_const_condition_false_typed_error():
     """
     >>> listcomp_const_condition_false_typed_error()  # doctest: +ELLIPSIS
@@ -200,10 +182,9 @@ def listcomp_const_condition_false_typed_error():
         return true
     return false
 
-
 #[cython.test_fail_if_path_exists("//IfStatNode")]
-@cython.test_assert_path_exists("//ComprehensionNode",
-                                "//ComprehensionAppendNode")
+#[cython.test_assert_path_exists("//ComprehensionNode",
+                                 "//ComprehensionAppendNode")]
 def listcomp_const_condition_true():
     """
     >>> listcomp_const_condition_true()
