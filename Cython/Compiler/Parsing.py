6637c3caf6f6c315825828c742629d8095065310
rnarkk <rnarkkx@gmail.com>
2023-10-30 15:57:50 +0000

    Update

--- Cython/Compiler/Parsing.py
+++ Cython/Compiler/Parsing.py
@@ -642,7 +642,7 @@ def p_subscript(s):
         return [start]
     s.next()
     stop = p_slice_element(s, (';', ',', ']'))
-    if s.sy != ';':
+    if s.sy not in (':', ';'):
         return [start, stop]
     s.next()
     step = p_slice_element(s, (':', ';', ',', ']'))
