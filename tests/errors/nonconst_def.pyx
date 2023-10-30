00d150815428301c0c94a16c812e8dceac66086c
Rin Arakaki <rnarkkx@gmail.com>
2023-10-30 13:49:01 +0900

    Update nonconst_def.pyx

--- tests/errors/nonconst_def.pyx
+++ tests/errors/nonconst_def.pyx
@@ -4,6 +4,6 @@ import os
 const ospath = os.path
 
 _ERRORS = u"""
-4:13: Compile-time name 'os' not defined
-4:15: Error in compile-time expression: AttributeError: 'NoneType' object has no attribute 'path'
+4:15: Compile-time name 'os' not defined
+4:17: Error in compile-time expression: AttributeError: 'NoneType' object has no attribute 'path'
 """
