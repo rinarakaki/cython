513ddb48b15289c5cf3619cc929daae11c4f6b04
Rin Arakaki <rnarkkx@gmail.com>
2023-10-31 16:06:09 +0900

    Update libc_all.pyx

--- tests/compile/libc_all.pyx
+++ tests/compile/libc_all.pyx
@@ -2,17 +2,17 @@
 
 use libc
 
-use libc::stdio
-use libc::errno
-use libc::float
-use libc::limits
-use libc::locale
-use libc::signal
-use libc::stddef
-# use libc::stdint  # XXX MSVC
-use libc::stdio
-use libc::stdlib
-use libc::string
+cimport libc.stdio
+cimport libc.errno
+cimport libc.float
+cimport libc.limits
+cimport libc.locale
+cimport libc.signal
+cimport libc.stddef
+# cimport libc.stdint  # XXX MSVC
+cimport libc.stdio
+cimport libc.stdlib
+cimport libc.string
 
 use libc::errno
 use libc::float
