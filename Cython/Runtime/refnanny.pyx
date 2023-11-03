7881512e67b8ed842e6d52e0594bfdfc6f977f5d
Rin Arakaki <rnarkkx@gmail.com>
2023-11-03 20:37:01 +0900

    Update refnanny.pyx

--- Cython/Runtime/refnanny.pyx
+++ Cython/Runtime/refnanny.pyx
@@ -62,7 +62,8 @@ cdef class Context(object):
     fn end(self):
         if self.refs:
             let msg = u"References leaked:"
-            let count, linenos
+            let i32 count
+            let list linenos
             for count, linenos in self.refs.itervalues():
                 msg += f"\n  ({count}) acquired on lines: {u', '.join([f'{x}' for x in linenos])}"
             self.errors.append(msg)
