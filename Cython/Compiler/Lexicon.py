0b1b13eb03c8862a05932fa51d77727edbeb171b
Rin Arakaki <rnarkkx@gmail.com>
2023-11-04 16:34:38 +0900

    Update Lexicon.py

--- Cython/Compiler/Lexicon.py
+++ Cython/Compiler/Lexicon.py
@@ -43,7 +43,8 @@ def make_lexicon():
     decimal = underscore_digits(digit)
     dot = Str(".")
     exponent = Any("Ee") + Opt(Any("+-")) + decimal
-    decimal_fract = (decimal + dot + Opt(decimal)) | (dot + decimal)
+    # decimal_fract = (decimal + dot + Opt(decimal)) | (dot + decimal)
+    decimal_fract = decimal + Str(".") + decimal
 
     # name = letter + Rep(letter | digit)
     name = Opt(Str("r#")) + unicode_start_character + Rep(unicode_continuation_character)
