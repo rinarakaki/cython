52c641d4b98d6dcd79a790ad996eea836066a231
Rin Arakaki <rnarkkx@gmail.com>
2023-11-05 15:56:00 +0900

    Update test_grammar.py

--- tests/run/test_grammar.py
+++ tests/run/test_grammar.py
@@ -170,10 +170,10 @@ INVALID_UNDERSCORE_LITERALS = [
     '1_.4',
     '1_.4j',
     # Underscore right after a dot:
-    '1._4',
-    '1._4j',
-    '0._5',
-    '0._5j',
+    # '1._4',  `_4` can be a method
+    # '1._4j',  `_4j` can be a method
+    '._5',
+    '._5j',
     # Underscore right after a sign:
     '1.0e+_1',
     '1.0e+_1j',
