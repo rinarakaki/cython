60367595d736691d8333b7f3e6c44f11d805663f
Rin Arakaki <rnarkkx@gmail.com>
2023-11-06 02:17:33 +0900

    Update gencall.pyx

--- tests/compile/gencall.pyx
+++ tests/compile/gencall.pyx
@@ -1,13 +1,12 @@
 # mode: compile
 
-def f(x, y):
+fn f(x, y):
     x = y
 
-def z(a, b, c):
+fn z(a, b, c):
     f(x = 42, y = "spam")
     f(*a)
     f(**b)
     f(x = 42, **b)
     f(a, *b)
     f(a, x = 42, *b, **c)
-
