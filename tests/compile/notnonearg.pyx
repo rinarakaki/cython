3fce8f7a1e24b3971feeb3e03702b71f30bc1124
Rin Arakaki <rnarkkx@gmail.com>
2023-11-06 02:26:48 +0900

    Update notnonearg.pyx

--- tests/compile/notnonearg.pyx
+++ tests/compile/notnonearg.pyx
@@ -3,5 +3,5 @@
 cdef extern class external.Spam [object Spam]: pass
 cdef extern class external.Eggs [object Eggs]: pass
 
-def ham(Spam s, Eggs e not None):
+fn ham(Spam s, Eggs e not None):
     pass
