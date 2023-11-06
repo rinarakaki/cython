ac3b13e15799f1cdb467b7e782d25b656e8485a9
Rin Arakaki <rnarkkx@gmail.com>
2023-11-07 01:13:07 +0900

    Update malloc.pyx

--- docs/examples/tutorial/memory_allocation/malloc.pyx
+++ docs/examples/tutorial/memory_allocation/malloc.pyx
@@ -1,7 +1,7 @@
 import random
 use libc::stdlib::(malloc, free)
 
-fn f64* random_noise(i32 number=1):
+fn list random_noise(i32 number=1):
     let i32 i
     # allocate number * sizeof(f64) bytes of memory
     let auto my_array = <f64 *>malloc(number * sizeof(f64))
