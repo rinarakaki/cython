ff8e9e7399ded8843a498185d939fd951b9387e1
Rin Arakaki <rnarkkx@gmail.com>
2023-11-06 13:25:36 +0900

    Update wrapper_vector.pyx

--- docs/examples/userguide/wrapping_CPlusPlus/wrapper_vector.pyx
+++ docs/examples/userguide/wrapping_CPlusPlus/wrapper_vector.pyx
@@ -5,10 +5,10 @@ use libcpp::vector::vector
 cdef class VectorStack:
     cdef vector[i32] v
 
-    def push(self, x):
+    fn push(self, x):
         self.v.push_back(x)
 
-    def pop(self):
+    fn pop(self):
         if self.v.empty():
             raise IndexError()
         x = self.v.back()
