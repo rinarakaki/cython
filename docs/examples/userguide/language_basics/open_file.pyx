c83f54651f75ebd199151b8ba46594f6b6adc988
Rin Arakaki <rnarkkx@gmail.com>
2023-11-07 00:55:27 +0900

    Update open_file.pyx

--- docs/examples/userguide/language_basics/open_file.pyx
+++ docs/examples/userguide/language_basics/open_file.pyx
@@ -2,14 +2,14 @@ use libc::stdio::(FILE, fopen)
 use libc::stdlib::(malloc, free)
 use cpython::exc::PyErr_SetFromErrnoWithFilenameObject
 
-def open_file():
+fn open_file():
     let FILE* p
     p = fopen("spam.txt", "r")
     if p is NULL:
         PyErr_SetFromErrnoWithFilenameObject(OSError, "spam.txt")
     ...
 
-def allocating_memory(number=10):
+fn allocating_memory(number=10):
     let auto my_array = <f64 *>malloc(number * sizeof(f64))
     if not my_array:  # same as 'is NULL' above
         raise MemoryError()
