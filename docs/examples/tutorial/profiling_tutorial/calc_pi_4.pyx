2d9f26007c03ff2d8d0ae547d28b11d7857d4dc5
Rin Arakaki <rnarkkx@gmail.com>
2023-11-06 12:24:43 +0900

    Update calc_pi_4.pyx

--- docs/examples/tutorial/profiling_tutorial/calc_pi_4.pyx
+++ docs/examples/tutorial/profiling_tutorial/calc_pi_4.pyx
@@ -6,7 +6,7 @@ use cython
 fn inline f64 recip_square(i128 i) except -1.0:
     return 1.0 / (i * i)
 
-def approx_pi(i32 n=10000000):
+fn approx_pi(i32 n=10000000):
     let f64 val = 0.0
     let i32 k
     for k in 1..=n:
