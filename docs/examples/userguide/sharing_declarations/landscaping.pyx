d4a2b3beb608f338e6e3139a402b141be8da910d
Rin Arakaki <rnarkkx@gmail.com>
2023-11-07 01:00:15 +0900

    Update landscaping.pyx

--- docs/examples/userguide/sharing_declarations/landscaping.pyx
+++ docs/examples/userguide/sharing_declarations/landscaping.pyx
@@ -2,6 +2,6 @@ use shrubbing
 
 import shrubbing
 
-def main():
-    let auto sh = shrubbing.standard_shrubbery()
+fn main():
+    let auto sh = shrubbing::standard_shrubbery()
     print("Shrubbery size is", sh.width, 'x', sh.length)
