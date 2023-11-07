08ea413f063a3aa3d44068f6174df6b81e8a7d81
rnarkk <rnarkkx@gmail.com>
2023-11-07 08:46:13 +0000

    Update

--- Cython/Compiler/Nodes.py
+++ Cython/Compiler/Nodes.py
@@ -517,7 +517,7 @@ class CNameDeclaratorNode(CDeclaratorNode):
         return self.name
 
     def analyse(self, base_type, env, nonempty=0, visibility=None, in_pxd=False):
-        if env.is_c_class_scope and base_type is None and self.name == "self":
+        if env is not None and env.is_c_class_scope and base_type is None and self.name == "self":
             base_type = env.parent_type
         elif nonempty and self.name == '':
             # May have mistaken the name for the type.
