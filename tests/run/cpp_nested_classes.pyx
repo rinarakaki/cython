7f24abf6102befcfef6931df20bfbaecba885153
Rin Arakaki <rnarkkx@gmail.com>
2023-11-07 10:36:20 +0900

    Update cpp_nested_classes.pyx

--- tests/run/cpp_nested_classes.pyx
+++ tests/run/cpp_nested_classes.pyx
@@ -8,6 +8,7 @@ extern from "cpp_nested_classes_support.h":
                 i32 cube(i32)
         B* createB()
         ctypedef i32 my_int
+        @staticmethod
         fn my_int negate(my_int)
 
     cdef cppclass TypedClass[T]:
@@ -34,6 +35,7 @@ cdef cppclass AA:
     BB* createB():
         return new BB()
     ctypedef i32 my_int
+    @staticmethod
     fn my_int negate(my_int x):
         return -x
 
