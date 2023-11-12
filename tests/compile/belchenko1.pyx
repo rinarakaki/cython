bd031ead061ccaddbc7cda2e279c24d68e599366
rnarkk <rnarkkx@gmail.com>
2023-11-12 17:08:57 +0000

    Update

--- tests/compile/belchenko1.pyx
+++ tests/compile/belchenko1.pyx
@@ -4,6 +4,6 @@ extern from *:
     type intptr_t = i32
 
 fn i32 _is_aligned(void *ptr):
-    return (<intptr_t>ptr & (sizeof(i32)) - 1) == 0
+    return (<intptr_t>ptr & (sizeof(i32) - 1)) == 0
 
 _is_aligned(NULL)
