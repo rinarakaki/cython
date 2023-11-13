85e46ece5c093e987f5b008980b387f732b76d0f
Rin Arakaki <rnarkkx@gmail.com>
2023-11-13 21:34:54 +0900

    Update union.pyx

--- docs/examples/userguide/language_basics/union.pyx
+++ docs/examples/userguide/language_basics/union.pyx
@@ -4,6 +4,6 @@ union Food:
 
 fn main():
     let f32 *arr = [1.0, 2.0]
-    let auto spam = Food(spam='b')
-    let auto eggs = Food(eggs=arr)
+    let Food spam = Food(spam='b')
+    let Food eggs = Food(eggs=arr)
     print(spam.spam, eggs.eggs[0])
