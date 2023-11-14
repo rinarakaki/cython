06ea4af988e4632f993284b23570181d89de5afa
Rin Arakaki <rnarkkx@gmail.com>
2023-11-14 14:54:32 +0900

    Update e_int_literals_py2.py

--- tests/errors/e_int_literals_py2.py
+++ tests/errors/e_int_literals_py2.py
@@ -2,15 +2,15 @@
 # cython: language_level=2
 
 def int_literals():
-    a = 1L  # ok
-    b = 10000000000000L  # ok
-    c = 1UL
-    d = 10000000000000UL
-    e = 10000000000000LL
+    a = 1i64  # ok
+    b = 10000000000000i64  # ok
+    c = 1u64
+    d = 10000000000000u64
+    e = 10000000000000i128
 
 
 _ERRORS = """
-7:8: illegal integer literal syntax in Python source file
-8:8: illegal integer literal syntax in Python source file
-9:8: illegal integer literal syntax in Python source file
+7:11: illegal integer literal syntax in Python source file
+8:11: illegal integer literal syntax in Python source file
+9:11: illegal integer literal syntax in Python source file
 """
