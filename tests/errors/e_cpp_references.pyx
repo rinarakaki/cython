584f9362c5f6999f75cb7bdd944a3a27d4beb45f
rnarkk <rnarkkx@gmail.com>
2023-11-19 06:50:40 +0000

    Update

--- tests/errors/e_cpp_references.pyx
+++ tests/errors/e_cpp_references.pyx
@@ -1,8 +1,8 @@
 # mode: error
 # tag: cpp, cpp11
 
-fn void foo(&object x): pass
-fn void bar(&&object x): pass
+fn void foo(&mut object x): pass
+fn void bar(&mut &mut object x): pass
 
 _ERRORS="""
 4:13: Reference base type cannot be a Python object
