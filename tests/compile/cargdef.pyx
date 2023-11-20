ca81f0310f779032a05bd9039f5075428c170b02
Rin Arakaki <rnarkkx@gmail.com>
2023-11-20 21:24:35 +0900

    Update cargdef.pyx

--- tests/compile/cargdef.pyx
+++ tests/compile/cargdef.pyx
@@ -6,7 +6,7 @@ fn f(obj, i32 i, f64 f, i8* s1, i8 s2[]):
 fn g(obj, i32 i, f64 f, i8* s1, i8 s2[]):
     pass
 
-fn do_g((object*)(object, i32, f64, i8*, i8*) func):
+fn do_g(object(object, i32, f64, r&i8, r&i8) func):
     return func(1, 2, 3.14159, "a", "b")
 
 do_g(&g)
