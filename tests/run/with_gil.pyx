99895d8724954672466ef5ae2d0f005622b3c590
Rin Arakaki <rnarkkx@gmail.com>
2023-11-20 11:23:36 +0900

    Update with_gil.pyx

--- tests/run/with_gil.pyx
+++ tests/run/with_gil.pyx
@@ -19,7 +19,7 @@ def redirect_stderr(func, *args, **kwargs):
     func(*args, **kwargs)
     sys.stderr = stderr
 
-fn void puts(r&char string) with gil:
+fn void puts(r&mut char string) with gil:
     """
     We need this for doctest, used from nogil sections.
     """
