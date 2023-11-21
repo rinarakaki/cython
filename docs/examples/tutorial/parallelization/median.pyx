4ca3646e8bf69d9aa1f6929cbd0a3510b3c2b1b1
Rin Arakaki <rnarkkx@gmail.com>
2023-11-22 01:11:12 +0900

    Update median.pyx

--- docs/examples/tutorial/parallelization/median.pyx
+++ docs/examples/tutorial/parallelization/median.pyx
@@ -9,7 +9,7 @@ import numpy as np
 
 #[cython::boundscheck(false)]
 #[cython::wraparound(false)]
-def median_along_axis0(const f64[:, :] x):
+fn median_along_axis0(const f64[:, :] x):
     let f64[:;1] out = np.empty(x.shape[1])
     let isize i, j
 
