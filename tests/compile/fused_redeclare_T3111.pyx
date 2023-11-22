f83f94ee4d8507939fbfe9651e8c6e968b66ff4b
rnarkk <rnarkkx@gmail.com>
2023-11-22 04:38:10 +0000

    Update

--- tests/compile/fused_redeclare_T3111.pyx
+++ tests/compile/fused_redeclare_T3111.pyx
@@ -27,8 +27,8 @@ _WARNINGS = """
 36:10: 'cpdef_cname_method' redeclared
 
 # from MemoryView.pyx
-954:29: Ambiguous exception value, same as default return value: 0
-954:29: Ambiguous exception value, same as default return value: 0
+952:29: Ambiguous exception value, same as default return value: 0
+952:29: Ambiguous exception value, same as default return value: 0
 995:46: Ambiguous exception value, same as default return value: 0
 995:46: Ambiguous exception value, same as default return value: 0
 1085:28: Ambiguous exception value, same as default return value: 0
--- tests/errors/pure_warnings.py
+++ tests/errors/pure_warnings.py
@@ -55,8 +55,8 @@ _WARNINGS = """
 # Spurious warnings from utility code - not part of the core test
 25:10: 'cpdef_method' redeclared
 36:10: 'cpdef_cname_method' redeclared
-954:29: Ambiguous exception value, same as default return value: 0
-954:29: Ambiguous exception value, same as default return value: 0
+952:29: Ambiguous exception value, same as default return value: 0
+952:29: Ambiguous exception value, same as default return value: 0
 995:46: Ambiguous exception value, same as default return value: 0
 995:46: Ambiguous exception value, same as default return value: 0
 1085:28: Ambiguous exception value, same as default return value: 0
