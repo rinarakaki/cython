4323773f66925c989d92ef89f050997ed190906e
rnarkk <rnarkkx@gmail.com>
2023-11-25 11:25:44 +0000

    Update

--- Cython/Compiler/Parsing.py
+++ Cython/Compiler/Parsing.py
@@ -2543,7 +2543,7 @@ def p_statement(s, ctx, first_statement = 0):
     if not s.in_python_file and (
         (s.sy != "IDENT" or s.systring == "type" or s.systring == "api" and s.peek()[0] in ("static", "fn", "type", "enum", "struct", "class"))
         and
-        (s.sy != "cdef" or s.peek()[0] == "class")
+        (s.sy != "cdef" or s.peek()[0] == ("extern", "enum", "struct", "union", "class"))
         and
         (s.sy not in ("pub", "readonly") or s.peek()[0] != "IDENT" or s.peek()[1] in ("packed", "api"))
     ):
