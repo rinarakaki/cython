a457b8d43f17d6c9170f32c4c6f393d67792eea9
rnarkk <rnarkkx@gmail.com>
2023-11-25 04:57:27 +0000

    Update

--- docs/examples/tutorial/parallelization/norm.pyx
+++ docs/examples/tutorial/parallelization/norm.pyx
@@ -4,7 +4,7 @@ use libc::math::sqrt
 
 #[cython::boundscheck(false)]
 #[cython::wraparound(false)]
-def l2norm(f64[:] x):
+fn l2norm(f64[:] x):
     let f64 total = 0
     let usize i
     for i in prange(x.shape[0], nogil=true):
