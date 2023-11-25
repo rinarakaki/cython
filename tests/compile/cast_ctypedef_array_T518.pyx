fa0cf06474310a5657cbc43e17f085d3173e8edf
rnarkk <rnarkkx@gmail.com>
2023-11-25 11:19:08 +0000

    Update

--- tests/compile/cast_ctypedef_array_T518.pyx
+++ tests/compile/cast_ctypedef_array_T518.pyx
@@ -4,15 +4,15 @@
 extern from "cast_ctypedef_array_T518_helper.h":
     struct __foo_struct:
        i32 i, j
-    ctypedef __foo_struct foo_t[1]
+    type foo_t = __foo_struct[1]
 
     fn void foo_init(foo_t)
     fn void foo_clear(foo_t)
 
-cdef foo_t value
+static foo_t value
 foo_init(value)
 foo_clear(value)
 
-cdef void* pointer = <void*>value
+static void* pointer = <void*>value
 foo_init(<foo_t>pointer)
 foo_clear(<foo_t>pointer)
