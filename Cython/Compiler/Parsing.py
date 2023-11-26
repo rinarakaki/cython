d7f69a6aea694d0668a0ee013324cf220c6a86f2
rnarkk <rnarkkx@gmail.com>
2023-11-26 08:09:15 +0000

    Update

--- Cython/Compiler/Parsing.py
+++ Cython/Compiler/Parsing.py
@@ -2519,7 +2519,7 @@ def p_item(s, ctx, attributes):
             error(pos, "Extension type definition not allowed here")
         if ctx.overridable:
             error(pos, "Extension types cannot be declared cpdef")
-        return p_c_class_definition(s, pos, ctx)
+        item = p_c_class_definition(s, pos, ctx)
 
 
     if item is not None:
