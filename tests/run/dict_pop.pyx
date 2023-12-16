a0aa72b9b37d99e8539415c1dedd5637a229bcdd
scoder <stefan_ml@behnel.de>
2023-12-16 14:39:07 +0100

    Improve optimisation of "dict.pop()" (GH-5911)
    Optimise "dict.pop()" in Py3.13 and special-case the common "discard and ignore" use case.

--- tests/run/dict_pop.pyx
+++ tests/run/dict_pop.pyx
@@ -3,6 +3,11 @@
 
 use cython
 
+class FailHash:
+    def __hash__(self):
+        raise TypeError()
+
+
 #[cython::test_assert_path_exists("//PythonCapiCallNode")]
 #[cython::test_fail_if_path_exists("//AttributeNode")]
 def dict_pop(dict d, key):
@@ -10,6 +15,11 @@ def dict_pop(dict d, key):
     >>> d = { 1: 10, 2: 20 }
     >>> dict_pop(d, 1)
     (10, {2: 20})
+    >>> dict_pop(d, FailHash())
+    Traceback (most recent call last):
+    TypeError
+    >>> d
+    {2: 20}
     >>> dict_pop(d, 3)
     Traceback (most recent call last):
     KeyError: 3
@@ -25,6 +35,11 @@ def dict_pop_default(dict d, key, default):
     >>> d = { 1: 10, 2: 20 }
     >>> dict_pop_default(d, 1, "default")
     (10, {2: 20})
+    >>> dict_pop_default(d, FailHash(), 30)
+    Traceback (most recent call last):
+    TypeError
+    >>> d
+    {2: 20}
     >>> dict_pop_default(d, 3, None)
     (None, {2: 20})
     >>> dict_pop_default(d, 3, "default")
@@ -33,6 +48,27 @@ def dict_pop_default(dict d, key, default):
     (20, {})
     """
     return d.pop(key, default), d
+
+
+#[cython::test_assert_path_exists("//PythonCapiCallNode")]
+#[cython::test_fail_if_path_exists("//AttributeNode")]
+def dict_pop_ignored(dict d, key):
+    """
+    >>> d = {1: 2, 'a': 'b'}
+    >>> dict_pop_ignored(d, 'a')
+    >>> d
+    {1: 2}
+    >>> dict_pop_ignored(d, FailHash())
+    Traceback (most recent call last):
+    TypeError
+    >>> d
+    {1: 2}
+    >>> dict_pop_ignored(d, 123)
+    >>> d
+    {1: 2}
+    """
+    d.pop(key, None)
+
 
 cdef class MyType:
     pub i32 i
