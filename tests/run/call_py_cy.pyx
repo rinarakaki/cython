8c3f9d37d300fe226f50fd7cd0b1492a1add3c9e
Rin Arakaki <rnarkkx@gmail.com>
2024-09-26 12:39:24 +0900

    Update call_py_cy.pyx

--- tests/run/call_py_cy.pyx
+++ tests/run/call_py_cy.pyx
@@ -5,9 +5,9 @@
 # #####
 # Test that Cython and Python functions can call each other in various signature combinations.
 # and check that the right calls use vectorcall (PyMethodCallNode)
-#######
+# ######
 
-cimport cython
+use cython
 
 py_call_noargs = eval("lambda: 'noargs'")
 py_call_onearg = eval("lambda arg: arg")
@@ -18,7 +18,7 @@ py_call_starstarargs = eval("lambda **kw: sorted(kw.items())")
 py_call_args_and_starstarargs = eval("lambda *args, **kw: (args, sorted(kw.items()))")
 
 
-#@cython.test_fail_if_path_exists("//PyMethodCallNode")
+# #[cython::test_fail_if_path_exists("//PyMethodCallNode")]
 def cy_call_noargs():
     """
     >>> cy_call_noargs()
@@ -27,7 +27,7 @@ def cy_call_noargs():
     return py_call_noargs()
 
 
-@cython.test_assert_path_exists("//PyMethodCallNode")
+#[cython::test_assert_path_exists("//PyMethodCallNode")]
 def cy_call_onearg(f):
     """
     >>> cy_call_onearg(py_call_onearg)
@@ -48,7 +48,7 @@ def cy_call_onearg(f):
     return f('onearg')
 
 
-@cython.test_assert_path_exists("//PyMethodCallNode")
+#[cython::test_assert_path_exists("//PyMethodCallNode")]
 def cy_call_twoargs(f, arg):
     """
     >>> cy_call_twoargs(py_call_twoargs, 132)
@@ -67,7 +67,7 @@ def cy_call_twoargs(f, arg):
     return f(arg, 'twoargs')
 
 
-@cython.test_assert_path_exists("//PyMethodCallNode")
+#[cython::test_assert_path_exists("//PyMethodCallNode")]
 def cy_call_arg_and_kwarg(f, arg):
     """
     >>> cy_call_arg_and_kwarg(py_call_twoargs, 123)
@@ -85,7 +85,7 @@ def cy_call_arg_and_kwarg(f, arg):
     return f(arg, arg2='twoargs')
 
 
-@cython.test_assert_path_exists("//PyMethodCallNode")
+#[cython::test_assert_path_exists("//PyMethodCallNode")]
 def cy_call_two_kwargs(f, arg):
     """
     >>> cy_call_two_kwargs(py_call_twoargs, arg=132)
@@ -104,7 +104,7 @@ def cy_call_two_kwargs(f, arg):
     return f(arg2='two-kwargs', arg=arg)
 
 
-@cython.test_fail_if_path_exists("//PyMethodCallNode")
+#[cython::test_fail_if_path_exists("//PyMethodCallNode")]
 def cy_call_starargs(*args):
     """
     >>> cy_call_starargs()
@@ -119,7 +119,7 @@ def cy_call_starargs(*args):
     return py_call_starargs(*args)
 
 
-@cython.test_fail_if_path_exists("//PyMethodCallNode")
+#[cython::test_fail_if_path_exists("//PyMethodCallNode")]
 def cy_call_pos_and_starargs(f, *args):
     """
     >>> cy_call_pos_and_starargs(py_call_onearg)
@@ -157,7 +157,7 @@ def cy_call_pos_and_starargs(f, *args):
 # Choice of whether to use PyMethodCallNode here is pretty arbitrary -
 # vectorcall_dict or PyObject_Call are likely to be fairly similar cost.
 # The test is for the current behaviour but it isn't a big issue if it changes
-@cython.test_fail_if_path_exists("//PyMethodCallNode")
+#[cython::test_fail_if_path_exists("//PyMethodCallNode")]
 def cy_call_starstarargs(**kw):
     """
     >>> kw = {}
@@ -176,7 +176,7 @@ def cy_call_starstarargs(**kw):
 # Choice of whether to use PyMethodCallNode here is pretty arbitrary -
 # vectorcall_dict or PyObject_Call are likely to be fairly similar cost.
 # The test is for the current behaviour but it isn't a big issue if it changes
-@cython.test_fail_if_path_exists("//PyMethodCallNode")
+#[cython::test_fail_if_path_exists("//PyMethodCallNode")]
 def cy_call_kw_and_starstarargs(f=None, arg1=None, **kw):
     """
     >>> kw = {}
@@ -237,7 +237,7 @@ def cy_call_kw_and_starstarargs(f=None, arg1=None, **kw):
     return (f or py_call_starstarargs)(arg=arg1, **kw)
 
 
-@cython.test_assert_path_exists("//PyMethodCallNode")
+#[cython::test_assert_path_exists("//PyMethodCallNode")]
 def cy_call_pos_and_starstarargs(f=None, arg1=None, **kw):
     """
     >>> cy_call_pos_and_starstarargs(arg=123)
